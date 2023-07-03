f5ceabf0507742b5c463589318eba70be7d38cca
bappy-ahashan <bappyahashan@gmail.com>
2023-07-03 22:47:34 +0600

    adding local storage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log('hellow world');
+const fieldSection = (idName) => {
+    const field = document.getElementById(idName);
+    const fieldValue = field.value;
+    return fieldValue;
+
+}
+
+const setLocalStorage = (value) => {
+    localStorage.setItem('name', value);
+}
+const setLocalStorageAge = (age) => {
+    localStorage.setItem('age', age)
+}
+
+const addName = () => {
+    const name = fieldSection('name-field');
+    setLocalStorage(name);
+
+}
+
+const addAge = () => {
+    const age = fieldSection('age-field');
+    setLocalStorageAge(age);
+}
+
+const removeName = () => {
+    localStorage.removeItem('name');
+}
+
+const removeAge = () => {
+    localStorage.removeItem('age');
+}
